581a208b35a6c6aa4e81ee3d128c2cd79341fbd9
sappusaketh <sakethkumarkappala@gmail.com>
2019-02-08 16:35:11 -0500

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Navbar,NavbarBrand} from 'reactstrap';
+import { Navbar,NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import { DISHES } from "./shared/dishes";
 import './App.css';
